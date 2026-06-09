e88fe17d69348a6b66c982d410465b377f6bf4fa
Chahatpreet Singh <c@chahat.dev>
2026-06-08 22:43:29 -0500

    Change neovim theme to vscode

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -1,76 +1,91 @@
 return {
-  {
-    'Mofiqul/adwaita.nvim',
-    lazy = false, -- load at start
-    priority = 1000, -- load first
-    config = function()
-      vim.g.adwaita_darker = true
-      vim.cmd [[colorscheme adwaita]]
-    end,
-  },
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false, -- load at start
+		priority = 1000, -- load first
+		opts = {
+			-- Enables transparent background
+			transparent = true,
 
-  -- Highlight todo, notes, etc in comments
-  {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    ---@module 'todo-comments'
-    ---@type TodoOptions
-    ---@diagnostic disable-next-line: missing-fields
-    opts = { signs = false },
-  },
+			-- Enables italic comment formatting
+			italic_comments = true,
 
-  -- nice bar at the bottom
-  {
-    'itchyny/lightline.vim',
-    lazy = false, -- also load at start since it's UI
-    config = function()
-      -- no need to also show mode in cmd line when we have bar
-      vim.o.showmode = false
-      vim.g.lightline = {
-        colorscheme = 'one',
-        active = {
-          left = {
-            { 'mode', 'paste' },
-            { 'readonly', 'filename', 'modified' },
-          },
-          right = {
-            { 'lineinfo' },
-            { 'percent' },
-            { 'fileencoding', 'filetype' },
-          },
-        },
-        component_function = {
-          filename = 'LightlineFilename',
-        },
-      }
-      function _G.LightlineFilenameInLua(opts)
-        if vim.fn.expand '%:t' == '' then
-          return '[No Name]'
-        else
-          return vim.fn.getreg '%'
-        end
-      end
-      -- https://github.com/itchyny/lightline.vim/issues/657
-      vim.api.nvim_exec(
-        [[
+			-- Enables underline for links
+			underline_links = true,
+
+			-- Disables background colors for file explorers
+			disable_nvimtree_bg = true,
+		},
+		config = function(_, opts)
+			-- 1. Setup the theme options first
+			require("vscode").setup(opts)
+			-- 2. Execute the colorscheme command
+			vim.cmd("colorscheme vscode")
+		end,
+	},
+
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		---@module 'todo-comments'
+		---@type TodoOptions
+		---@diagnostic disable-next-line: missing-fields
+		opts = { signs = false },
+	},
+
+	-- nice bar at the bottom
+	{
+		"itchyny/lightline.vim",
+		lazy = false, -- also load at start since it's UI
+		config = function()
+			-- no need to also show mode in cmd line when we have bar
+			vim.o.showmode = false
+			vim.g.lightline = {
+				colorscheme = "one",
+				active = {
+					left = {
+						{ "mode", "paste" },
+						{ "readonly", "filename", "modified" },
+					},
+					right = {
+						{ "lineinfo" },
+						{ "percent" },
+						{ "fileencoding", "filetype" },
+					},
+				},
+				component_function = {
+					filename = "LightlineFilename",
+				},
+			}
+			function _G.LightlineFilenameInLua(opts)
+				if vim.fn.expand("%:t") == "" then
+					return "[No Name]"
+				else
+					return vim.fn.getreg("%")
+				end
+			end
+			-- https://github.com/itchyny/lightline.vim/issues/657
+			vim.api.nvim_exec(
+				[[
 				function! g:LightlineFilename()
 					return v:lua.LightlineFilenameInLua()
 				endfunction
 				]],
-        true
-      )
-    end,
-  },
+				true
+			)
+		end,
+	},
 
-  { -- Collection of various small independent plugins/modules
-    'nvim-mini/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      require('mini.ai').setup { n_lines = 500 }
+	{ -- Collection of various small independent plugins/modules
+		"nvim-mini/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			require("mini.ai").setup({ n_lines = 500 })
 
-      -- Add/delete/replace surroundings
-      require('mini.surround').setup()
-    end,
-  },
+			-- Add/delete/replace surroundings
+			require("mini.surround").setup()
+		end,
+	},
 }
