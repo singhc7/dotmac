995f7d9f70ad6a3cacf4e20bac0c6c9d5453b771
Chahatpreet Singh <c@chahat.dev>
2026-06-09 21:28:30 -0500

    Update lsp(s), linters and formatters in nvim

--- nvim/.config/nvim/lua/custom/plugins/indent.lua
+++ nvim/.config/nvim/lua/custom/plugins/indent.lua
@@ -2,10 +2,10 @@
 ---@type LazySpec
 return {
   { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+    "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
-    main = 'ibl',
+    main = "ibl",
     ---@module 'ibl'
     ---@type ibl.config
     opts = {},
